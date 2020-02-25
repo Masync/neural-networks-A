af375bc6bd9239161a5efd476b6b19511e3217c4
Juan Manuel Patiño <jumapa662@gmail.com>
2020-02-24 23:30:05 -0500

    refactorizacion de codigo
    refactorizacion de codigo

--- js/xnor.js
+++ js/xnor.js
@@ -1,52 +1,58 @@
-var Main = () => {
-  //* Obteniendo la data del Html
-  // *Declarando Variables de la and
-  // *Declarando Variables de la Nor
-  let w1 = parseFloat(document.getElementById("peso1").value);
-  let _w1 = parseFloat(document.getElementById("_peso1").value);
-  let w2 = parseFloat(document.getElementById("peso2").value);
-  let _w2 = parseFloat(document.getElementById("_peso2").value);
-  var umbral = parseFloat(document.getElementById("umbral").value);
-  var _umbral = parseFloat(document.getElementById("_umbral").value);
+function main() {
+      let w1 = parseFloat(document.getElementById("peso1").value);
+      let _w1 = parseFloat(document.getElementById("_peso1").value);
+      let w2 = parseFloat(document.getElementById("peso2").value);
+      let _w2 = parseFloat(document.getElementById("_peso2").value);
+      var umbral = parseFloat(document.getElementById("umbral").value);
+      var _umbral = parseFloat(document.getElementById("_umbral").value);
 
-  //* Logica de compuerta and
-  var resulAnd = (((1 * w1) + (1 * w2) + (1 * umbral)));
+      
+      function validation(data) {
+        data.toFixed(1);
+        if (data >= 0.5) {
+           return data = 1;
+          } else {
+           return data = 0;
+          }
+          
+        }
+        function and(comp1,w1,comp2,w2,comp3,umbral) {
+          let data = (((comp1 *w1) + (comp2 *w2) + (comp3 *umbral)));
+          return _rA1 = validation(data);
 
-  //* Resultado con 1 decimal
-  resulAnd.toFixed(1);
-  //* Condicional para que la salida sea 1 | 0
-  if (resulAnd >= 0.5) {
-    resulAnd = 1;
-  } else {
-    resulAnd = 0;
-  }
-  //* Logica de compuerta Nor
-  var resulNor = (((1 * (-_w1)) + (1 * (-_w2)) + (1 * _umbral)));
+        };
+        function nor(comp1,_w1,comp2,_w2,comp3,_umbral) {
+          let data = (((comp1 * (-_w1)) + (comp2 * (-_w2)) + (comp3 * _umbral)));
+          return resul = validation(data);
+      
+        };
+        function Or(comp1,resulAnd1,comp2,resulNor1,comp3,umbral) {
+          let data = ((comp1 * resulAnd1) + (comp2 * resulNor1) + (comp3 * umbral));
+          return resul = validation(data);
+        };
+        function resultado(data,dataname) {
+          return document.getElementById(`${dataname}`).value = data;
+          
+        }
+      let and1 = and(0,w1,0,w2,1,umbral);
+      let and2 = and(0,w1,1,w2,0,umbral);
+      let and3 = and(1,w1,0,w2,0,umbral);
+      let and4 = and(1,w1,1,w2,1,umbral);
 
-  //* Resultado con 1 decimal
-  resulNor.toFixed(1);
+      let nor1 = nor(0,_w1,0,_w2,1,_umbral);
+      let nor2 = nor(0,_w1,1,_w2,0,_umbral);
+      let nor3 = nor(1,_w1,0,_w2,0,_umbral);
+      let nor4 = nor(1,_w1,1,_w2,0,_umbral);
 
-  //* Condicional para que la salida sea 1 | 0
-  if (resulNor >= 0.5) {
-    resulNor = 1;
-  } else {
-    resulNor = 0;
-  }
-  //* Llamado a la funcion  resultadoOr(); pasandole los resultado de and y Nor
-  resultadoOr(resulAnd, resulNor, umbral);
+      let _or1 = Or(0,and1,0,nor1,1,umbral);
+      let _or2 = Or(0,and2,0,nor2,1,umbral);
+      let _or3 = Or(0,and3,0,nor3,1,umbral);
+      let _or4 = Or(0,and4,0,nor4,1,umbral);
 
-};
-resultadoOr = (x, y, z) => {
-  //* Logica de compuerta Or
-  var resulData = ((0 * x) + (0 * y) + (1 * z));
-  //* Resultado con 1 decimal
-  resulData.toFixed(1);
-  //* Condicional para que la salida sea 1 | 0
-  if (resulData >= 0.5) {
-    resulData = 1;
-  } else {
-    resulData = 0;
-  }
-  //* Agregando la data del resultado al HTML
-  document.getElementById("resFinal").value = resulData;
-};
+      resultado(_or1,'resul1');
+      resultado(_or2,'resul1');
+      resultado(_or3,'resul1');
+      resultado(_or4,'resul1');
+
+      
+      };
