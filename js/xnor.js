b3dc3edfbd6fe44b201deab1a7e9e3ee37d22821
Juan Manuel Patiño <jumapa662@gmail.com>
2020-02-26 17:51:18 -0500

    correcion de compuerta

--- js/xnor.js
+++ js/xnor.js
@@ -14,7 +14,7 @@ function main() {
   // *  @param {number} y return a bool 0|1
   function validation(data) {
     data.toFixed(1);
-    if (data >= 0.5) {
+    if (data >= 0.6) {
        return data = 1;
       } else {
        return data = 0;
@@ -29,36 +29,46 @@ function main() {
 
     // * @param {bool,number} y return resultado de la compuerta NOR
     function nor(comp1,_w1,comp2,_w2,comp3,_umbral) {
-      let data = (((comp1 * (-_w1)) + (comp2 * (-_w2)) + (comp3 * _umbral)));
+      let data = (((comp1 + (-_w1)) + (comp2 + (-_w2)) + (comp3 + _umbral)));
       return resul = validation(data);
   
     };
     // * @param {bool,number} y return resultado de la compuerta  AND y NOR
     function Or(comp1,resulAnd1,comp2,resulNor1,comp3,umbral) {
-      let data = ((comp1 * resulAnd1) + (comp2 * resulNor1) + (comp3 * umbral));
+      let data = ((comp1 + resulAnd1) + (comp2 + resulNor1) + (comp3 + umbral));
       return resul = validation(data);
     };
      // * @param {string,number} y return el resultado de las compuerta XNOR en HTML
     function resultado(dataname,data) {
       return document.getElementById(dataname).value = data; 
     };
+     // * @param {string,number} y return el resultado de las compuerta XNOR en HTML
+     function cumple(dataname,data, data2) {
+       if (data === data2) {
+        document.getElementById(dataname).style.border = '2px solid #21bf73';
+         return document.getElementById(dataname).value = "Cumple"; 
+       } else {
+        document.getElementById(dataname).style.border = '2px solid #e32249';
+        return document.getElementById(dataname).value = "No cumple"; 
+       }
+    };
   
   //* Cargando valores en la funcion and()
   let and1 = and(0,w1,0,w2,1,umbral);
-  let and2 = and(0,w1,1,w2,0,umbral);
-  let and3 = and(1,w1,0,w2,0,umbral);
+  let and2 = and(0,w1,1,w2,1,umbral);
+  let and3 = and(1,w1,0,w2,1,umbral);
   let and4 = and(1,w1,1,w2,1,umbral);
 
   //* Cargando valores en la funcion nor()
   let nor1 = nor(0,_w1,0,_w2,1,_umbral);
-  let nor2 = nor(0,_w1,1,_w2,0,_umbral);
-  let nor3 = nor(1,_w1,0,_w2,0,_umbral);
-  let nor4 = nor(1,_w1,1,_w2,0,_umbral);
+  let nor2 = nor(0,_w1,1,_w2,1,_umbral);
+  let nor3 = nor(1,_w1,0,_w2,1,_umbral);
+  let nor4 = nor(1,_w1,1,_w2,1,_umbral);
 
   //* Cargando valores en la funcion Or() con los resulados de and() y nor()
   let _or1 = Or(0,and1,0,nor1,1,umbral);
-  let _or2 = Or(0,and2,1,nor2,0,umbral);
-  let _or3 = Or(1,and3,0,nor3,0,umbral);
+  let _or2 = Or(0,and2,1,nor2,1,umbral);
+  let _or3 = Or(1,and3,0,nor3,1,umbral);
   let _or4 = Or(1,and4,1,nor4,1,umbral);
   
   //* Cargando resultados en la funcion  resultado()
@@ -67,4 +77,9 @@ function main() {
   resultado("r3",_or3);
   resultado("r4",_or4);
 
+    //* Cargando resultados en la funcion  cumple()
+  cumple("v1",_or1,1);
+  cumple("v2",_or2,0);
+  cumple("v3",_or3,0);
+  cumple("v4",_or4,1);
   };
--- js/xor.js
+++ js/xor.js
@@ -1,40 +1,88 @@
+// * Funcion Principal
+function main() {
+  /**
+  *  @let Variables Locales
+  *  @var Varibles Globales
+  **/
+  let w1 = parseFloat(document.getElementById("peso1").value);
+  let _w1 = parseFloat(document.getElementById("_peso1").value);
+  let w2 = parseFloat(document.getElementById("peso2").value);
+  let _w2 = parseFloat(document.getElementById("_peso2").value);
+  var umbral = parseFloat(document.getElementById("umbral").value);
+  var _umbral = parseFloat(document.getElementById("_umbral").value);
 
-        function Main() {
-          var w1 = document.getElementById("peso1").value;
-          var w2 = document.getElementById("peso2").value;
-          var umbral = document.getElementById("umbral").value;
-           
-          compuertaAnd(w1,w2,umbral);
-           
-         }
-         
-           function compuertaAnd(w1, w2, umbral) {
-       
-             var resultadoAnd;
-             resultadoAnd = parseFloat(((0*w1)+(1*w2)+(1*umbral)));
-             console.log(`El resultado de la compuerta And es ${resultadoAnd}`);
-             CompuertaNor(resultadoAnd,w2,umbral);
-             return resultadoAnd;
-       
-           }
-       
-           function CompuertaNor(resultadoAnd,w2, umbral) {
-             var resultadoNor;
-             resultadoNor = parseFloat(((0*(-resultadoAnd))+(1*(-w2))+(1*umbral)));
-             console.log(`El resultado de la compuerta Nor es ${resultadoNor}`);
-             CompuertaOr(resultadoNor,w2,umbral);
-             return resultadoNor;
-           }
-       
-           function CompuertaOr(resultadoNor,w2,umbral) {
-             var resultadoOr;
-             resultadoOr = parseFloat(((0*resultadoNor)+(1*w2)+(1*umbral)));
-             console.log(`El resultado de la compuerta Or es ${resultadoOr}`);
-             document.getElementById("resFinal").value = resultadoOr;
-             return resultadoOr;
-            
-           }
-       
-           
-       
+  // *  @param {number} y return a bool 0|1
+  function validation(data) {
+    data.toFixed(1);
+    if (data >= 0.6) {
+       return data = 1;
+      } else {
+       return data = 0;
+      }        
+    };
+
+    // * @param {bool,number} y return resultado de la compuerta AND
+    function and(comp1,w1,comp2,w2,comp3,umbral) {
+      let data = (((comp1 *w1) + (comp2 *w2) + (comp3 *umbral)));
+      return resul = validation(data);
+    };
+
+    // * @param {bool,number} y return resultado de la compuerta NAND
+    function nand(comp1,_w1,comp2,_w2,comp3,_umbral) {
+      let data = (((comp1 * (-_w1)) + (comp2 * (-_w2)) + (comp3 * _umbral)));
+      return resul = validation(data);
+  
+    };
+    // * @param {bool,number} y return resultado de la compuerta  AND y NAND
+    function Or(comp1,resulAnd1,comp2,resulNand1,comp3,umbral) {
+      let data = ((comp1 + resulAnd1) + (comp2 + resulNand1) + (comp3 + umbral));
+      return resul = validation(data);
+    };
+     // * @param {string,number} y return el resultado de las compuerta XOR en HTML
+    function resultado(dataname,data) {
+      return document.getElementById(dataname).value = data; 
+    };
+     // * @param {string,number} y return el resultado de las compuerta XOR en HTML
+     function cumple(dataname,data, data2) {
+       if (data === data2) {
+        document.getElementById(dataname).style.border = '2px solid #21bf73';
+         return document.getElementById(dataname).value = "Cumple"; 
+       } else {
+        document.getElementById(dataname).style.border = '2px solid #e32249';
+        return document.getElementById(dataname).value = "No cumple"; 
+       }
+    };
+  
+  //* Cargando valores en la funcion and()
+  let and1 = and(0,w1,0,w2,1,umbral);
+  let and2 = and(0,w1,1,w2,1,umbral);
+  let and3 = and(1,w1,0,w2,1,umbral);
+  let and4 = and(1,w1,1,w2,1,umbral);
+
+  //* Cargando valores en la funcion nand()
+  let nand1 = nand(0,_w1,0,_w2,1,_umbral);
+  let nand2 = nand(0,_w1,1,_w2,1,_umbral);
+  let nand3 = nand(1,_w1,0,_w2,1,_umbral);
+  let nand4 = nand(1,_w1,1,_w2,1,_umbral);
+
+  //* Cargando valores en la funcion Or() con los resulados de and() y nand()
+  let _or1 = Or(0,and1,0,nand1,1,umbral);
+  let _or2 = Or(0,and2,1,nand2,1,umbral);
+  let _or3 = Or(1,and3,0,nand3,1,umbral);
+  let _or4 = Or(1,and4,1,nand4,1,umbral);
+  
+  //* Cargando resultados en la funcion  cumple()
+  resultado("r1",_or1);
+  resultado("r2",_or2);
+  resultado("r3",_or3);
+  resultado("r4",_or4);
+
+    //* Cargando resultados en la funcion  resultado()
+  cumple("v1",_or1,1);
+  cumple("v2",_or2,0);
+  cumple("v3",_or3,0);
+  cumple("v4",_or4,1);
+
+
+  };
            
